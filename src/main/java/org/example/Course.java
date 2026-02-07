1b5b292cc71f5277e77625bf021acb0aee55319f
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-02-07 08:50:09 +0800

    Added: Course Class

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Course {
+    private String courseID;
+    private String courseName;
+    private String program;
+
+    public Course(){}
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public String getProgram(){
+        return program;
+    }
+
+    public void setCourseID(String courseID){
+        this.courseID = courseID;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+}
