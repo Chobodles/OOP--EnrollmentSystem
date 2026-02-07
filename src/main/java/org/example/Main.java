ad45eb456ee1c84c44309c5bea82b03b3cae006c
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-02-07 08:58:23 +0800

    Added: Print output in Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,36 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Course c = new Course();
 
+        s1.setStudentID("000123");
+        s1.setStudentName("John Doe");
+        s1.setProgram("Information Technology");
+
+        s2.setStudentID("000124");
+        s2.setStudentName("Jane Doe");
+        s2.setProgram("Information Technology");
+
+        c.setCourseID("0001");
+        c.setCourseName("Integrative Programming");
+        c.setProgram("Information Technology");
+
+        System.out.println(s1.getStudentID());
+        System.out.println(s1.getStudentName());
+        System.out.println(s1.getProgram());
+        System.out.println();
+
+        System.out.println(s2.getStudentID());
+        System.out.println(s2.getStudentName());
+        System.out.println(s2.getProgram());
+        System.out.println();
+
+        System.out.println(c.getCourseID());
+        System.out.println(c.getCourseName());
+        System.out.println(c.getProgram());
+        System.out.println();
 
     }
 }
